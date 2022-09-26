73064833b7bac1dcba40a3c5146ff77b4182d12b
Eugenio De Simone <eugenio12simone@gmail.com>
2022-09-27 00:38:30 +0200

    Working on the model

--- training/model_training.py
+++ training/model_training.py
@@ -12,14 +12,13 @@ def read_kmers(input_file):
         kmers_list = [] #list
         kmers_encoded = {} #dictionary
         for lines in csvFile:
-            #print(lines['id_gene'])
-            kmers_list.append((lines['id_gene'],lines['kmer']))
+            X.append(ohe(lines['kmer']))
+            y.append(lines['id_gene'])
 
-    for elem in kmers_list:
-        kmers_encoded[elem[0]] = ohe(elem[1])
-        #print(kmers_encoded)
-    return kmers_encoded
+    return X, y
 
+def training():
+    pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -31,8 +30,26 @@ if __name__ == '__main__':
 
     read_kmers(args.input_file)
 
-    #model = tf.keras.models.load_model('newdat_newmod_jj.h5')
-    #print(model.summary())
+    model = tf.keras.models.load_model('newdat_newmod_jj.h5')
+    ##print(model.summary())
+    layer = model.get_layer(index=0)
 
-    #test
+    """
+    print(dir(y))
+    print(y.filters) # (256)
+    print(y.kernel_size) # (20 , 4)
+    print(y.activation) # relu
+    print(y.input_shape) # (None, 20000, 4, 1)
+    print(y.output_shape)
+    """
 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=787)
+    print(X_train.shape, X_train.shape[1:])
+    model = tf.keras.models.Sequential()
+
+    model.add(Conv2D(256, kernel_size=(20, 4), activation='relu', input_shape=X_train.shape[:]))
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+    history = model.fit(X_train, y_train, batch_size=8, epochs=10, validation_data=(X_test, y_test))
+    results = model.eveluate(X_test, y_test, batch_size=8)
