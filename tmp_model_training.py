7ae07baddf184f446a55ca19015531337a48f8ee
MusimathicsLab <dev.cirillo@gmail.com>
2022-11-01 04:34:34 +0100

    added lstm

--- tmp_model_training.py
+++ tmp_model_training.py
@@ -5,14 +5,17 @@ import pandas as pd
 import pickle
 import tensorflow as tf
 
-from keras.backend import image_data_format
-from keras.layers import Conv2D, Flatten, Dense
+from keras import Sequential
+from keras.layers import Dense, Embedding, Bidirectional, LSTM
 from keras.utils import to_categorical
-from training.encoding import one_hot_encode as ohe
-from utils.file import create_training_path_file, create_test_path_file, create_conf_path_file
+from keras_preprocessing.sequence import pad_sequences
+from keras_preprocessing.text import Tokenizer
+from sklearn.model_selection import train_test_split
 
+from utils.file import create_training_path_file, create_conf_path_file
 
-def encode2train(input_file: str, code_function):
+"""
+def encode2train(input_file: str, code_function): 
     df = pd.read_csv(input_file, header=0, engine="python")
     df_seq = df.iloc[:, 1]
     X = np.array([code_function(x) for x in df_seq])
@@ -43,7 +46,7 @@ def create_model(_n_classes):
     _model.add(Dense(_n_classes, activation='softmax'))
 
     return _model
-
+"""
 
 if __name__ == '__main__':
 
@@ -66,61 +69,48 @@ if __name__ == '__main__':
     with open(conf_path_file, "rb") as conf_file:
         conf = pickle.load(conf_file)
 
-    # Load if exists test file
-    test_path_file = ""
-    if conf["split"]:
-        test_path_file = create_test_path_file(args.input_file)
-        if not os.path.isfile(test_path_file):
-            print("No such file.")
-            print("Please run pre-processing.py first")
-
-    # Get X, y and classes of training
-    X_train, y_train, classes_train = encode2train(training_path_file, ohe)
+    # Get X and y of training
+    df = pd.read_csv(training_path_file, header=0, engine="python")
+    id_genes = np.array(df.iloc[:, 0])
+    kmers = np.array(df.iloc[:, 1])
 
     # Encode label train
     classes = conf["classes"]
     n_classes = conf["n_classes"]
     label_encoder = conf["label-encoder"]
-    y_train_encoded = label_encoder.transform(y_train)
-    y_train_encoded = to_categorical(y_train_encoded, num_classes=n_classes)
+    id_genes_encoded = label_encoder.transform(id_genes)
+    id_genes_encoded = to_categorical(id_genes_encoded, num_classes=n_classes)
 
-    X_train = reshape_data(X_train)
+    # Tokenizing kmers
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(kmers)
+    encoded_kmers = tokenizer.texts_to_sequences(kmers)
+    max_length = max([len(s.split()) for s in kmers])
+    kmers = pad_sequences(encoded_kmers, maxlen=max_length, padding='post')
 
-    print("Dimension of data: " + str(X_train.shape))
-    print(f"Num of training classes: {len(classes_train)}")
+    # Split dataset in training and validation set
+    kmers_train, kmers_val, id_genes_train, id_genes_val = train_test_split(
+        kmers, id_genes_encoded, test_size=0.25, random_state=42)
 
-    """
-    model = create_model(n_classes)
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-    model.summary()
-    model.fit(X_train, y_train_encoded, batch_size=128, epochs=2)
+    vocab_size = len(tokenizer.word_index) + 1
+
+    model = Sequential()
+    model.add(Embedding(vocab_size, 20))
+    model.add(Bidirectional(LSTM(20)))
+    model.add(Dense(20, activation='relu'))
+    model.add(Dense(n_classes, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=1e-4, patience=10)
+    ]
+
+    model.fit(kmers_train,
+              id_genes_train,
+              validation_data=(kmers_val, id_genes_val),
+              batch_size=128,
+              epochs=200,
+              callbacks=callbacks)
 
     model.save('./training/gene-fusion-model.h5', save_format='h5')
-    """
-
-    model = tf.keras.models.load_model('./training/gene-fusion-model.h5')
-
-    # Test if split is True
-    if conf["split"]:
-        # Load csv and get X and y
-        df = pd.read_csv(test_path_file, header=0, engine="python")
-        df_seq = df.iloc[:, 1]
-        y = np.array(df.iloc[:, 0])
-
-        # For each element
-        for i in range(len(y)):
-            print(f"Label to predict: {y[i]}")
-            # Get kmer
-            sequence = df_seq[i]
-            n_kmers = (len(sequence) - conf["k-size"] + 1)
-            kmers = [sequence[j:j + conf["k-size"]] for j in range(n_kmers)]
-            X_test = np.array([ohe(kmer) for kmer in kmers])
-            X_test = reshape_data(X_test)
-
-            # Create y_train encoded
-            y_train_encoded = np.full(n_kmers, y[i])
-            y_train_encoded = label_encoder.transform(y_train_encoded)
-            y_train_encoded = to_categorical(y_train_encoded, num_classes=n_classes)
-
-            results = model.evaluate(X_test, y_train_encoded, batch_size=128)
-            print("test loss, test acc:", results)
